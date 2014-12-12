ba43f43e24ac7e63edd2f48ce08bd35459a92a06
Wim Smit <wimsmit@Dev-iMac.local>
2014-12-12 17:23:06 +0100

    Raw working prototype ready

--- Waar Ben Ik/ViewController.swift
+++ Waar Ben Ik/ViewController.swift
@@ -11,11 +11,50 @@ import CoreLocation
 
 class ViewController: UIViewController,CLLocationManagerDelegate {
     
+    @IBOutlet weak var streetName: UILabel!
+    @IBOutlet weak var zip: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var course: UILabel!
+    @IBOutlet weak var speed: UILabel!
+    @IBOutlet weak var altitude: UILabel!
+    
     var manager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        
+        var userLocation: CLLocation = locations[0] as CLLocation
+        // streetName.text = "\(userLocation.coordinate.latitude)"
+        course.text = "\(userLocation.course)"
+        speed.text = "\(userLocation.speed * 1.609)"
+        altitude.text = "\(userLocation.altitude)"
+        
+        CLGeocoder().reverseGeocodeLocation(userLocation, completionHandler:{(placemarks, NSError) in
+        
+            if NSError != nil { println(NSError)}
+            else    {
+                let p = CLPlacemark(placemark: placemarks?[0] as CLPlacemark)
+                
+                self.streetName.text = "\(p.thoroughfare) " + "\(p.subThoroughfare)"
+                self.city.text = p.locality
+                self.country.text = p.country
+                self.zip.text = p.postalCode
+                
+
+            }
+        })
         
         
     }
