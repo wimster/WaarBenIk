1d28490f13c7dfe2f496e377bc7875a67a62b8a9
Wim Smit <wimsmit@Dev-iMac.local>
2014-12-12 14:52:42 +0100

    Initial Commit

---
+++ Waar Ben Ik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Waar Ben Ik
+//
+//  Created by Wim Smit on 12/12/14.
+//  Copyright (c) 2014 ITMOTION. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
